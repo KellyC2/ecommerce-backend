ed8446e499685a90a1c7d6acb93ea6fc1dc69a43
Kelly Condori <kellycondoricondori@gmail.com>
2024-09-29 13:50:18 -0500

    We create the project structure and our first entity in our model folder

---
+++ src/main/java/com/kellyprojects/ecommerce_project/model/Product.java
@@ -0,0 +1,29 @@
+package com.kellyprojects.ecommerce_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private double id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private LocalDate releaseDate;
+    private  boolean available;
+    private int quantity;
+
+}
