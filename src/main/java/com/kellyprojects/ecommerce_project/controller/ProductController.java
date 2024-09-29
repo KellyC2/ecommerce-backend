412e4b09d8c506cf83690b81c3b718ed2cad9619
Kelly Condori <kellycondoricondori@gmail.com>
2024-09-29 17:39:09 -0500

    We connect fronted with the backend

--- src/main/java/com/kellyprojects/ecommerce_project/controller/ProductController.java
+++ src/main/java/com/kellyprojects/ecommerce_project/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.kellyprojects.ecommerce_project.controller;
 import com.kellyprojects.ecommerce_project.model.Product;
 import com.kellyprojects.ecommerce_project.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
     private ProductService service;
